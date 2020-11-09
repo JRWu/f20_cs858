6b571813de3d906f9ab3993c7fc64c342a347f3d
Jia Wu <jwu424@gmail.com>
2020-11-08 22:34:28 -0500

    Updating with preliminary exploratory work.

--- src/AnalyzePrivacyPolicy.py
+++ src/AnalyzePrivacyPolicy.py
@@ -4,11 +4,36 @@ NAIVE text ranking. Explore a document at the phrase level.
 
 """
 
+from os import listdir
+from os.path import isfile, join
+import glob
 
 import pytextrank
 import spacy
 
 data_dir = "/f20_cs858/data/"
+#all_policies = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
+
+all_policies = glob.glob(data_dir + '*.txt')
+
+
+for pol in all_policies:
+	print(pol)
+
+
+
+p_test = all_policies[0]
+
+
+
+
+
+
+
+
+
+
+
 pp_15 = "15_sleep_science_alarm_clock.txt"
 
 with open(data_dir + pp_15, 'r') as file:
@@ -26,3 +51,5 @@ doc = nlp(data_pp_15)
 for p in doc._.phrases:
     print("{:.4f} {:5d}  {}".format(p.rank, p.count, p.text))
     print(p.chunks)
+
+
